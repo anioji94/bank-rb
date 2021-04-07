8b50fd04ad897b9ffb1f33443b348e8b4fb83a8e
Aniruddh <ojhaa94@gmail.com>
2021-04-07 12:30:24 +0100

    adds deposit function

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,11 +1,15 @@
 class Bank
 	attr_reader :balance
 	def initialize
-		@balance = 0
+		@balance = 0.00
 	end
 
 	def print
 		puts "date || credit || debit || balance"
-		puts " ||  ||  || #{@balance} "
+		puts " ||  ||  || #{sprintf "%.2f", @balance} "
+	end
+
+	def deposit(amount)
+	 @balance += amount.to_f
 	end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,8 +4,14 @@ describe Bank do
 	describe '#print' do
 		context 'when no withdrawals or deposits have been made' do
 			it 'should print an empty statement' do
-				expect{ subject.print }.to output("date || credit || debit || balance\n ||  ||  || 0 \n").to_stdout
+				expect{ subject.print }.to output("date || credit || debit || balance\n ||  ||  || 0.00 \n").to_stdout
 			end
+		end
+	end
+
+	describe '#deposit' do
+		it 'should change the balance of the account' do
+			expect{ subject.deposit(200) }.to change{ subject.balance }
 		end
 	end
 end
