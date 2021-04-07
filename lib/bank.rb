3ae39179a01c9cbe441d35041bfb60758083610a
Aniruddh <ojhaa94@gmail.com>
2021-04-07 13:01:32 +0100

    adds deposit amount

--- lib/bank.rb
+++ lib/bank.rb
@@ -2,14 +2,22 @@ class Bank
 	attr_reader :balance
 	def initialize
 		@balance = 0.00
+		@dep_amount = ""
+
 	end
 
 	def print
 		puts "date || credit || debit || balance"
-		puts " ||  ||  || #{sprintf "%.2f", @balance} "
+		puts " || #{@dep_amount} ||  || #{sprintf "%.2f", @balance} "
 	end
 
 	def deposit(amount)
-	 @balance += amount.to_f
+		@balance += amount.to_f
+		@dep_amount = sprintf "%.2f", amount
 	end
+
+	def withdraw(amount)
+		@balance -= amount.to_f
+	end
+
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -7,11 +7,27 @@ describe Bank do
 				expect{ subject.print }.to output("date || credit || debit || balance\n ||  ||  || 0.00 \n").to_stdout
 			end
 		end
+		
+		context'when a deposit is made' do
+			it 'should store deposit amount' do
+				subject.deposit(200)
+				expect{ subject.print }.to output("date || credit || debit || balance\n || 200.00 ||  || 200.00 \n").to_stdout
+			end
+		end
 	end
 
 	describe '#deposit' do
-		it 'should change the balance of the account' do
+		it 'should increase the balance of the account by 200' do
 			expect{ subject.deposit(200) }.to change{ subject.balance }.by(200)
 		end
 	end
+
+	describe '#withdraw' do
+		it 'should decrease the balance of the account by 200' do
+			subject.deposit(200)
+			expect{ subject.withdraw(200) }.to change{ subject.balance }.by(-200)
+		end
+	end
+
+	
 end
