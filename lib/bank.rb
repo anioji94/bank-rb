527f2490d14ffb559e697aab3fcab189fd5e6633
Aniruddh <ojhaa94@gmail.com>
2021-04-07 18:40:06 +0100

    Almost all rubocop offenses corrected

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,48 +1,45 @@
 class Bank
-	attr_reader :balance, :dep_amount, :with_amount, :history
-	def initialize
-		@balance = 0.00
-		@dep_amount = ""
-		@with_amount = ""
-		@history = []
-		@statement = ""
-		@time = "#{Time.now.day}/#{Time.now.month}/#{Time.now.year}"
-		save
-	end
+  attr_reader :balance, :dep_amount, :with_amount, :history
 
-	def print_statement
-		write_statement
-		puts "date || credit || debit || balance\n" + @statement
-		@statement = ""
-	end
+  def initialize
+    @balance = 0.00
+    @dep_amount = ''
+    @with_amount = ''
+    @history = []
+    @statement = ''
+    @time = "#{Time.now.day}/#{Time.now.month}/#{Time.now.year}"
+    save
+  end
 
-	def deposit(amount)
-		@balance += amount.to_f
-		@dep_amount = sprintf "%.2f", amount
-		save(@dep_amount, "", @balance)
-	end
+  def print_statement
+    write_statement
+    puts "date || credit || debit || balance\n#{@statement}"
+    @statement = ''
+  end
 
-	def withdraw(amount)
-		@dep_amount = ""
-		@balance -= amount.to_f
-		@with_amount = sprintf "%.2f", amount
-		save("", @with_amount, @balance)
-	end
+  def deposit(amount)
+    @balance += amount.to_f
+    @dep_amount = format '%.2f', amount
+    save(@dep_amount, '', @balance)
+  end
 
-private 
+  def withdraw(amount)
+    @dep_amount = ''
+    @balance -= amount.to_f
+    @with_amount = format '%.2f', amount
+    save('', @with_amount, @balance)
+  end
 
-	def save(dep_amount = "", with_amount = "", balance = @balance, time = @time)
-		transaction = " #{time} || #{dep_amount} || #{with_amount} || #{sprintf "%.2f", balance} "
-		@history.unshift(transaction)
-	end
+  private
 
-	def write_statement
-		@history.each do |transaction|
-			@statement += "#{transaction}\n"
-		end
-	end
+  def save(dep_amount = '', with_amount = '', balance = @balance, time = @time)
+    transaction = " #{time} || #{dep_amount} || #{with_amount} || #{format '%.2f', balance} "
+    @history.unshift(transaction)
+  end
 
-	
-		
-	
+  def write_statement
+    @history.each do |transaction|
+      @statement += "#{transaction}\n"
+    end
+  end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,42 +1,46 @@
 require 'bank'
 
 describe Bank do
-	let(:time) {"#{Time.now.day}/#{Time.now.month}/#{Time.now.year}"}
-	describe '#print' do
-		context 'when no withdrawals or deposits have been made' do
-			it 'should print an empty statement' do
-				expect{ subject.print_statement }.to output("date || credit || debit || balance\n #{time} ||  ||  || 0.00 \n").to_stdout
-			end
-		end
-		
-		context'when a deposit is made' do
-			it 'should store deposit amount' do
-				subject.deposit(200)
-				expect{ subject.print_statement }.to output("date || credit || debit || balance\n #{time} || 200.00 ||  || 200.00 \n #{time} ||  ||  || 0.00 \n").to_stdout
-			end
-		end
+  let(:time) { "#{Time.now.day}/#{Time.now.month}/#{Time.now.year}" }
+  describe '#print' do
+    context 'when no withdrawals or deposits have been made' do
+      it 'should print an empty statement' do
+        expect do
+          subject.print_statement
+        end.to output("date || credit || debit || balance\n #{time} ||  ||  || 0.00 \n").to_stdout
+      end
+    end
 
-		context'when a withdrawal is made' do
-			it 'should store withdrawal amount' do
-				subject.deposit(200)
-				subject.withdraw(10)
-				expect{ subject.print_statement }.to output("date || credit || debit || balance\n #{time} ||  || 10.00 || 190.00 \n #{time} || 200.00 ||  || 200.00 \n #{time} ||  ||  || 0.00 \n").to_stdout
-			end
-		end
-	end
+    context 'when a deposit is made' do
+      it 'should store deposit amount' do
+        subject.deposit(200)
+        expect do
+          subject.print_statement
+        end.to output("date || credit || debit || balance\n #{time} || 200.00 ||  || 200.00 \n #{time} ||  ||  || 0.00 \n").to_stdout
+      end
+    end
 
-	describe '#deposit' do
-		it 'should increase the balance of the account by 200' do
-			expect{ subject.deposit(200) }.to change{ subject.balance }.by(200)
-		end
-	end
+    context 'when a withdrawal is made' do
+      it 'should store withdrawal amount' do
+        subject.deposit(200)
+        subject.withdraw(10)
+        expect do
+          subject.print_statement
+        end.to output("date || credit || debit || balance\n #{time} ||  || 10.00 || 190.00 \n #{time} || 200.00 ||  || 200.00 \n #{time} ||  ||  || 0.00 \n").to_stdout
+      end
+    end
+  end
 
-	describe '#withdraw' do
-		it 'should decrease the balance of the account by 200' do
-			subject.deposit(200)
-			expect{ subject.withdraw(200) }.to change{ subject.balance }.by(-200)
-		end
-	end
+  describe '#deposit' do
+    it 'should increase the balance of the account by 200' do
+      expect { subject.deposit(200) }.to change { subject.balance }.by(200)
+    end
+  end
 
-	
+  describe '#withdraw' do
+    it 'should decrease the balance of the account by 200' do
+      subject.deposit(200)
+      expect { subject.withdraw(200) }.to change { subject.balance }.by(-200)
+    end
+  end
 end
